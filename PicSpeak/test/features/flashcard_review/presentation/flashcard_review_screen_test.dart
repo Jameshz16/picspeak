b0f9814cefe650444133cfaf02812d49450f6d18
Jameshz16 <jhon.james.1995@gmail.com>
2026-08-03 20:59:59 -0500

    test: fix 5 pre-existing test failures from SRS feature (fakes + rounding)

--- PicSpeak/test/features/flashcard_review/presentation/flashcard_review_screen_test.dart
+++ PicSpeak/test/features/flashcard_review/presentation/flashcard_review_screen_test.dart
@@ -48,6 +48,20 @@ class _MockFlashcardRepository implements FlashcardRepository {
       _cards.add(word);
     }
   }
+
+  @override
+  Future<List<RecognizedWord>> getDueCards() async => _cards;
+
+  @override
+  Future<void> updateSrs({
+    required String enLabel,
+    required int interval,
+    required double easeFactor,
+    required DateTime nextReview,
+  }) async {}
+
+  @override
+  Future<int> getDueCount() async => _cards.length;
 }
 
 void main() {
